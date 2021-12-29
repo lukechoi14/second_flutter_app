bd992c8fdbf0d38c50f9b429add6177d1718fdb4
luke choi <primera_jh@hotmail.com>
2021-12-29 17:45:22 +0900

    first page

--- lib/detail_audio_page.dart
+++ lib/detail_audio_page.dart
@@ -15,13 +15,14 @@ class _DetailAudioPageState extends State<DetailAudioPage> {
     final double screenHeight= MediaQuery.of(context).size.height;
     final double screenWidth= MediaQuery.of(context).size.width;
     return Scaffold(
+      backgroundColor: AppColors.audioBluishBackground,
       body: Stack(
         children: [
           Positioned(
               top: 0,
               left: 0,
               right: 0,
-              height: screenHeight/5,
+              height: screenHeight/3,
               child: Container(
                 color: AppColors.audioBlueBackground,
 
@@ -43,7 +44,60 @@ class _DetailAudioPageState extends State<DetailAudioPage> {
                 ],
                 backgroundColor: Colors.transparent,
                 elevation: 0.0,
-              ))
+              )),
+          Positioned(
+              left: 0,
+              right: 0,
+              top: screenHeight*0.2,
+              height: screenHeight*0.36,
+              child:Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(40),
+                  color: Colors.white,
+                ),
+                child: Column(
+                  children: [
+                    SizedBox(height: screenHeight*0.1,),
+                    Text("THE WATER CURE",style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "Avenir"
+                    ),),
+                    Text("Martin Hyatt",style:TextStyle(
+                      fontSize: 20,
+
+                    ),)
+                  ],
+                ),
+              )),
+          Positioned(
+              top: screenHeight*0.12,
+              left: (screenWidth-150)/2,
+              // width: 150,
+              right: (screenWidth-150)/2,
+              height: screenHeight*0.16,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: AppColors.audioGreyBackground,
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(color: Colors.white, width: 2)
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        // borderRadius: BorderRadius.circular(20),
+                        shape: BoxShape.circle,
+                        border: Border.all(color: Colors.white, width: 5),
+                        image: DecorationImage(
+                          image: AssetImage("img/pic-1.png"),
+                          fit: BoxFit.cover
+                        )
+                    ),
+                  ),
+                ),
+
+          ))
         ],
       ),
     );
